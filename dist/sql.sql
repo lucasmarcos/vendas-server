fcc985fb770f3c79674f0bc9df80ff4fb8b63b05
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-09-23 19:30:12 -0300

    tabelas

--- dist/sql.sql
+++ dist/sql.sql
@@ -7,10 +7,27 @@ CREATE TABLE IF NOT EXISTS usuario (
   nome TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS contato (
+CREATE TABLE IF NOT EXISTS cliente (
   id INTEGER PRIMARY KEY,
   nome TEXT,
-  telefone TEXT,
+  documento TEXT,
+  tipo INTEGER,
   email TEXT,
   endereco TEXT
+);
+
+CREATE TABLE IF NOT EXISTS fornecedor (
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS produto (
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS venda (
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS funcionario (
+  id INTEGER PRIMARY KEY
 );
