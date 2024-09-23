8c0c7aa3c04d8aa5f0a517f5c969d1d547b1ed69
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-09-23 18:25:00 -0300

    sql

--- dist/sql.sql
+++ dist/sql.sql
@@ -2,7 +2,12 @@ PRAGMA journal_mode=WAL;
 
 CREATE TABLE IF NOT EXISTS usuario (
   id INTEGER PRIMARY KEY,
-  email TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
   senha TEXT NOT NULL,
   nome TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contato (
+  id INTEGER PRIMARY KEY,
+  nome TEXT
 );
