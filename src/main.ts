2bc248413b4dd5a0853a554ae7eb59f9546789e1
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-09-23 18:25:07 -0300

    register

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,26 @@ const app = new Hono();
 
 app.post("/login", async (ctx) => {
   const { email, senha } = await ctx.req.json();
-  const sttm = db.prepare("SELECT id FROM usuario WHERE email = ? AND senha = ?;");
-  const res = sttm.get(email, senha) as any;
+  const sttm = db.prepare(
+    "SELECT id FROM usuario WHERE email = ? AND senha = ?;",
+  );
+  const res = sttm.get(email, senha) as { id: number };
   return ctx.json({ id: res.id });
+});
+
+app.post("/register", async (ctx) => {
+  const { email, senha, nome } = await ctx.req.json();
+  const sttm = db.prepare(
+    "INSERT INTO usuario (email, senha, nome) VALUES (?, ?, ?);",
+  );
+  const res = sttm.run(email, senha, nome);
+  return ctx.json({ id: res.lastInsertRowid });
+});
+
+app.post("/usuarios", async (ctx) => {
+  const sttm = db.prepare("SELECT id, email, nome FROM usuario;");
+  const res = sttm.all();
+  return ctx.json(res);
 });
 
 const PORT = Number.parseInt(env.PORT) || 3031;
